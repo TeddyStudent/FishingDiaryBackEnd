56998baef428ecdbf5a4ecaae9bcc0437407ba03
TeddyStudent <teukka@bastu.net>
2018-05-04 16:03:02 +0300

    All CRUD operations added to index.js

--- index.js
+++ index.js
@@ -34,25 +34,67 @@ app.get('/api/reissut/:reissuId',(req,res) => {
 //reissu object should be in the request body
 app.post('/api/reissut',(req,res) => {
 
-    //lets use joi to validate body data
-    const schema = {
-        name: Joi.string().min(1).max(40).required()
-    };
-    const result = Joi.validate(req.body, schema);
+    //validate data, return 400 if error
+    const { error } = validoiReissu(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
     
-    //if validation fails return the error
-    if (result.error){
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
-    
+    //create the new object
     const reissu = {
         id: reissut.length + 1,
         name: req.body.name
     };
     reissut.push(reissu);
+
+    //Return the object
     res.send(reissu);
 });
+
+
+//Lets listen PUT requests on 'localhost:<port>/api/reissut/<id of reissu object>' to modify an reissu object
+app.put('/api/reissut/:reissuId',(req,res) => {
+
+    //find the object, return 404 if error
+    const reissu = reissut.find(c => c.id === parseInt(req.params.reissuId));
+    if (!reissu) return res.status(404).send('Object was not found!');
+
+    //validate data, return 400 if error
+    const { error } = validoiReissu(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //modify the object
+    reissu.name = req.body.name;
+
+    //return modified object
+    res.send(reissu);
+});
+
+
+
+//Lets listen DELETE requests on 'localhost:<port>/api/reissut/<id of reissu object>' to delete a reissu object
+app.delete('/api/reissut/:reissuId',(req,res) => {
+
+    //find the object, return 404 if error
+    const reissu = reissut.find(c => c.id === parseInt(req.params.reissuId));
+    if (!reissu) return res.status(404).send('Object was not found!');
+
+    //delete the object
+    const index = reissut.indexOf(reissu);
+    reissut.splice(index,1);
+
+    //return the same reissu
+    res.send(reissu);
+    
+});
+
+
+//to avoid multiplying validation code lets use this function for reissu object
+function validoiReissu(reissu){
+    //use Joi to define validation rules for the name parameter
+    const schema = {
+        name: Joi.string().min(1).max(40).required()
+    };
+    return Joi.validate(reissu, schema);
+};
 
 
 //use the environment variable PORT if available, otherwise assign port to 3000
